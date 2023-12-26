7ab3474117e3e1cc5d0aef4f587a13503ea1f62c
דבורה עינב <enavamirim@gmail.com>
2023-12-26 18:41:18 +0200

    Intial Code +Welcome functions

---
+++ Stage0/Program8080.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Targil10
+{
+     partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome8080();
+            Welcome0359();
+            Console.ReadKey();
+
+        }
+
+        private static void Welcome8080()
+        {
+            Console.Write("Enter your name: ");
+            String username = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first application", username);
+        }
+        static partial void Welcome0359();
+    }
+}
