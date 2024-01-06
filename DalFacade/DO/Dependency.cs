6f8ccb520d8a16390e12fc10fb945ec05203b655
ריקי רובין <rrubin@g.jct.ac.il>
2024-01-07 00:21:03 +0200

    stage 1 chp 3

---
+++ DalFacade/DO/Dependency.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace DO;
+
+public record Dependency
+(   int id,
+    int dependentTask,
+    int dependsOnTesk
+)
+{
+ public Dependency() : this(0) { }
+public Dependency(int i, int dT, int dOT)
+{ this.id = i;
+        this.dependentTask = dT;
+        this.dependsOnTesk = dOT;
+}}
---
+++ DalFacade/DO/Engineer.cs
@@ -0,0 +1,28 @@
+﻿
+namespace DO;
+/// <summary>
+/// 
+/// </summary>
+/// <param name="id">Personal unique ID of the Engineer</param>
+/// <param name="fullName"></param>
+/// <param name="mail"></param>
+/// <param name="level"></param>
+/// <param name="payPerHour"></param>
+public record Engineer
+(int id,
+  string fullName,
+  string mail,
+  int level,
+  double payPerHour
+)
+{
+    public Engineer() : this(0) { }
+    public Engineer(int i, string n, string m, int l, double p)
+    {
+        this.id = i;
+        this.fullName = n;
+        this.mail = m;
+        this.level = l;
+        this.payPerHour = p;
+    }
+}
