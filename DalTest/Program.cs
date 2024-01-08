c8102dcd565936fe683e71ef159f738666a8e7be
דבורה עינב <enavamirim@gmail.com>
2024-01-08 23:25:10 +0200

    PROGRAM

--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DalApi;
+using DalTest;
+namespace DalTest
+{
+    internal class Program
+    {
+        private static IEngineer? eg = new EngineerImlementation(); //stage 1
+        private static ITask? t1 = new TaskImlementation(); //stage 1
+        private static IDependency? d1 = new DependencyImlementation(); //stage 1
+        public static void Main(string[] args)
+        {
+            Initialization.Do(eg, t1, d1);
+            string
+
+
+        }
+    }
+}
