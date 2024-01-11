38d94eff1fd8cfe2c2b2bf6c2c98c71b60781f1d
דבורה עינב <enavamirim@gmail.com>
2024-01-11 19:01:27 +0200

    test

--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -1,18 +1,1 @@
-﻿using DalApi;
-using DalTest;
-namespace DalTest
-{
-    internal class Program
-    {
-        private static IEngineer? eg = new EngineerImlementation(); //stage 1
-        private static ITask? t1 = new TaskImlementation(); //stage 1
-        private static IDependency? d1 = new DependencyImlementation(); //stage 1
-        public static void Main(string[] args)
-        {
-            Initialization.Do(eg, t1, d1);
-            string
-
-
-        }
-    }
-}
+﻿
