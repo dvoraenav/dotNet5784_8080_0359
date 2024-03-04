0eab3dcaf2df9d93031e3de36ba90dee6fb87243
דבורה עינב <enavamirim@gmail.com>
2024-03-05 01:28:16 +0200

    blimplemention bi clock

--- BL/BlImplementation/Bl.cs
+++ BL/BlImplementation/Bl.cs
@@ -8,11 +8,18 @@ internal class Bl : IBl
     public IEngineer Engineer => new EngineerImplementation();
     public ITask Task => new TaskImplementation();
 
+    public IClock Clock => throw new NotImplementedException();
+
     public void InitializeDB() => DalTest.Initialization.Do();
 
-    public void ResetDB()//TODO
-    {
-      DalTest.Initialization.Reset();
-    }
+    public void ResetDB()=> DalTest.Initialization.Reset();
+    private static DateTime s_Clock = DateTime.Now.Date;
+    public DateTime Clock_ { get { return s_Clock; }private set { s_Clock = value; } }
+    public void AdvanceTimeByYear() { Clock_ = Clock_.AddYears(1); }
+    public void AdvanceTimeByMounse() { Clock_ = Clock_.AddSeconds(1); }
+    public void AdvanceTimeByDay() { Clock_ = Clock_.AddDays(1); }
+    public void AdvanceTimeByHour() { Clock_ = Clock_.AddHours(1); }
+    public void ResetTime_() { Clock_=DateTime.Now.Date; }
+    
 
 }
